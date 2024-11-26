4a5372eadc05db67f1dddbe28ef031ac2433e88c
Remy Charpentier <remycha@msn.com>
2024-11-26 06:32:57 +0100

    bg gradient

--- src/components/styles/Main.styled.jsx
+++ src/components/styles/Main.styled.jsx
@@ -2,16 +2,27 @@ import styled from "styled-components";
 
 export const StyledMain = styled.div`
   min-height: 54vh;
-  background-image: linear-gradient(rgba(0, 0, 0, -1), rgba(0, 0, 0, 1)),
-    linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, -1)),
-    url(/src/assets/img/game13.jpg), url(/src/assets/img/game14.jpg);
-  background-repeat: repeat, repeat, no-repeat, repeat;
-  background-position: top;
-  background-size: 100% 1488px, 100% 1488px, 139%, 139%;
-  background-position-x: center, center, right, left;
-  background-position-y: top, top, top, top;
+  background-image: url(/src/assets/img/game13.jpg),
+    linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0.88) 35%,
+      rgba(0, 0, 0, 1) 40%,
+      rgba(0, 0, 0, 1) 60%,
+      rgba(0, 0, 0, 0.98) 61%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    url(/src/assets/img/game14.jpg);
+  background-repeat: no-repeat, no-repeat, no-repeat;
+  background-size: 139%, 100% 200px, 139%;
+  background-position-x: right, center, left;
+  background-position-y: 0, 1410px, 1488px;
+  background-blend-mode: multiply;
+  color: #f2f2f2;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-shadow: 0 2px 10px #000;
   #main-container {
-    color: #f2f2f2;
     padding: 16px 12px;
   }
 `;
--- src/pages/User/Games.jsx
+++ src/pages/User/Games.jsx
@@ -1,11 +1,8 @@
-import React from "react";
-
 export default function Games() {
   return (
     <>
       <h1>Page Jeux</h1>
       <h1 className="wip">⚒ A faire : liste de jeux et leur description</h1>
-      <APIProvider></APIProvider>
     </>
   );
 }
