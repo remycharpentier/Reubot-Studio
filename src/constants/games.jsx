4a0e7028140a4d9edb824c926dfbbe88c02558f6
Remy Charpentier <remycha@msn.com>
2024-11-27 10:51:20 +0100

    ajout de fichiers constants

---
+++ src/constants/games.jsx
@@ -0,0 +1,43 @@
+import imgGame1 from "../assets/img/front_game1.jpg";
+import imgGame2 from "../assets/img/front_game2.jpeg";
+import imgGame3 from "../assets/img/front_game3.jpg";
+import imgGame4 from "../assets/img/front_game4.jpg";
+const games = [
+  {
+    id: 1,
+    titre: "Kunitsu-Gami: Path of the Godess",
+    img: imgGame1,
+    genre: "Action / Aventure",
+    mode: "Solo",
+    support: "PC, PlayStation 5, Xbox Series X/S",
+    price: 59.99,
+  },
+  {
+    id: 2,
+    titre: "Monster Hunter Stories 2 : Wings of ruin",
+    img: imgGame2,
+    genre: "RPG (Jeu de rôle) / Tour par tour",
+    mode: "Solo et multijoueur",
+    support: "Nintendo Switch, PC",
+    price: 19.99,
+  },
+  {
+    id: 3,
+    titre: "Street Fighter 6",
+    img: imgGame3,
+    genre: "Jeu de combat",
+    mode: "Solo et multijoueur",
+    support: "PlayStation 4, PlayStation 5, Xbox Series X/S, PC",
+    price: 39.99,
+  },
+  {
+    id: 4,
+    titre: "Devil May Cry 5",
+    img: imgGame4,
+    genre: "Action / Hack and Slash",
+    mode: "Solo",
+    support: "PlayStation 4, PlayStation 5, Xbox One, Xbox Series X/S, PC",
+    price: 29.99,
+  },
+];
+export default games;
--- src/pages/User/Games.jsx
+++ src/pages/User/Games.jsx
@@ -1,47 +1,5 @@
-import game1 from "../../assets/img/front_game1.jpg";
-import game2 from "../../assets/img/front_game2.jpeg";
-import game3 from "../../assets/img/front_game3.jpg";
-import game4 from "../../assets/img/front_game4.jpg";
 import { StyledGames } from "../../components/styles/Games.styled";
-
-const games = [
-  {
-    id: 1,
-    titre: "Kunitsu-Gami: Path of the Godess",
-    img: game1,
-    genre: "Action / Aventure",
-    mode: "Solo",
-    support: "PC, PlayStation 5, Xbox Series X/S",
-    price: 59.99,
-  },
-  {
-    id: 2,
-    titre: "Monster Hunter Stories 2 : Wings of ruin",
-    img: game2,
-    genre: "RPG (Jeu de rôle) / Tour par tour",
-    mode: "Solo et multijoueur",
-    support: "Nintendo Switch, PC",
-    price: 19.99,
-  },
-  {
-    id: 3,
-    titre: "Street Fighter 6",
-    img: game3,
-    genre: "Jeu de combat",
-    mode: "Solo et multijoueur",
-    support: "PlayStation 4, PlayStation 5, Xbox Series X/S, PC",
-    price: 39.99,
-  },
-  {
-    id: 4,
-    titre: "Devil May Cry 5",
-    img: game4,
-    genre: "Action / Hack and Slash",
-    mode: "Solo",
-    support: "PlayStation 4, PlayStation 5, Xbox One, Xbox Series X/S, PC",
-    price: 29.99,
-  },
-];
+import games from "../../constants/games";
 
 export default function Games() {
   return (
