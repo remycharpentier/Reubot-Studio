0f268b71f5ca28b718015988bbfee6afe25f3483
Remy Charpentier <remycha@msn.com>
2024-11-28 06:19:25 +0100

    ajout router file

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  RouterProvider,
-  createBrowserRouter,
-  Navigate,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { StyledBody } from "./components/styles/Body.styled";
+import { StyledGlobal } from "./components/styles/Global.styled";
 import Header from "./components/ui/Header";
 import Main from "./components/ui/Main";
 import Footer from "./components/ui/Footer";
-import { StyledBody } from "./components/styles/Body.styled";
-import { StyledGlobal } from "./components/styles/Global.styled";
-import Home from "./pages/User/Home"; //
-import Games from "./pages/User/Games";
-import News from "./pages/User/News";
-import AboutUs from "./pages/User/AboutUs";
-import Contact from "./pages/User/Contact";
-import PageError from "./pages/User/PageError";
-
-const generateRoutes = () => [
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <PageError />,
-    children: [
-      { path: "home", element: <Home /> },
-      { path: "games", element: <Games /> },
-      { path: "news", element: <News /> },
-      { path: "about-us", element: <AboutUs /> },
-      { path: "contact", element: <Contact /> },
-      { path: "/", element: <Navigate to="/home" replace /> },
-    ],
-  },
-];
-
-const router = createBrowserRouter(generateRoutes());
+import createRouter from "./routes/router";
 
 const Root = () => (
   <>
@@ -44,6 +17,8 @@ const Root = () => (
     </StyledBody>
   </>
 );
+
+const router = createRouter(Root);
 
 const App = () => <RouterProvider router={router} />;
 
