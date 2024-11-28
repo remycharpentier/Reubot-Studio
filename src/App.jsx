65d5689c2239c32264fee9d08f51d8325263c15b
Remy Charpentier <remycha@msn.com>
2024-11-28 06:12:42 +0100

    simplification app

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,50 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+} from "react-router-dom";
 import Header from "./components/ui/Header";
 import Main from "./components/ui/Main";
 import Footer from "./components/ui/Footer";
 import { StyledBody } from "./components/styles/Body.styled";
 import { StyledGlobal } from "./components/styles/Global.styled";
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
-import Home from "./pages/User/Home";
+import Home from "./pages/User/Home"; //
 import Games from "./pages/User/Games";
 import News from "./pages/User/News";
 import AboutUs from "./pages/User/AboutUs";
 import Contact from "./pages/User/Contact";
 import PageError from "./pages/User/PageError";
 
-const router = createBrowserRouter([
+const generateRoutes = () => [
   {
     path: "/",
     element: <Root />,
     errorElement: <PageError />,
     children: [
-      {
-        path: "home",
-        element: <Home />,
-      },
-      {
-        path: "games",
-        element: <Games />,
-      },
-      {
-        path: "news",
-        element: <News />,
-      },
-      {
-        path: "about-us",
-        element: <AboutUs />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-      {
-        path: "/",
-        element: <Navigate to="/home" replace />,
-      },
+      { path: "home", element: <Home /> },
+      { path: "games", element: <Games /> },
+      { path: "news", element: <News /> },
+      { path: "about-us", element: <AboutUs /> },
+      { path: "contact", element: <Contact /> },
+      { path: "/", element: <Navigate to="/home" replace /> },
     ],
   },
-]);
+];
 
-function Root() {
-  return (
-    <>
-      <StyledGlobal />
-      <StyledBody>
-        <Header />
-        <Main />
-        <Footer />
-      </StyledBody>
-    </>
-  );
-}
+const router = createBrowserRouter(generateRoutes());
 
-function App() {
-  return <RouterProvider router={router} />;
-}
+const Root = () => (
+  <>
+    <StyledGlobal />
+    <StyledBody>
+      <Header />
+      <Main />
+      <Footer />
+    </StyledBody>
+  </>
+);
+
+const App = () => <RouterProvider router={router} />;
 
 export default App;
