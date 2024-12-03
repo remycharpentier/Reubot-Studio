ff80ce62ba86087163197a96eb55cd631ce59a10
Remy Charpentier <remycha@msn.com>
2024-12-03 14:58:01 +0100

    creation const team

---
+++ src/constants/Team.jsx
@@ -0,0 +1,85 @@
+import female1 from "../assets/img/team/female1.jpg";
+import female2 from "../assets/img/team/female2.jpg";
+import female3 from "../assets/img/team/female3.jpg";
+import female4 from "../assets/img/team/female4.jpg";
+import female5 from "../assets/img/team/female5.jpg";
+import male1 from "../assets/img/team/male1.jpg";
+import male2 from "../assets/img/team/male2.jpg";
+import male3 from "../assets/img/team/male3.jpg";
+import male4 from "../assets/img/team/male4.jpg";
+
+const Team = [
+  {
+    id: 1,
+    lastName: "Leroux",
+    name: "Émilie",
+    img: female1,
+    role: "Directrice artistique principale",
+    text: "Émilie est une directrice artistique principale avec plus de 15 ans d'expérience dans l'industrie du jeu vidéo. Elle a contribué à la conception artistique de nombreux jeux à succès, notamment sur des titres comme The Elder Scrolls V: Skyrim et God of War. Passionnée par l'esthétique visuelle et l'expérience utilisateur, elle combine son expertise en design graphique avec sa compréhension des dynamiques de jeu pour créer des univers immersifs. Émilie est impatiente de relever de nouveaux défis créatifs chez Reubot Studio.",
+  },
+  {
+    id: 2,
+    lastName: "Dupont",
+    name: "Julien",
+    img: male1,
+    role: "Chef de projet",
+    text: "Julien est le chef de projet de l'équipe, avec plus d'une décennie d'expérience dans la gestion de projets de jeux vidéo. Il est responsable de la coordination entre les différentes équipes et de la planification des délais. Sa passion pour le jeu et son sens de l'organisation font de lui un leader respecté au sein de Reubot Studio.",
+  },
+  {
+    id: 3,
+    lastName: "Bernard",
+    name: "Lucas",
+    img: male2,
+    role: "Programmeur principal",
+    text: "Lucas est un programmeur principal avec un fort accent sur les mécaniques de jeu et l'optimisation des moteurs. Avec plus de 12 ans d'expérience dans l'industrie, il a travaillé sur divers projets, y compris Call of Duty et Assassin's Creed. Son esprit analytique et ses compétences en résolution de problèmes font de lui un atout précieux pour l'équipe de développement chez Reubot Studio.",
+  },
+  {
+    id: 4,
+    lastName: "Lemoine",
+    name: "Antoine",
+    img: male3,
+    role: "Concepteur narratif",
+    text: "Antoine est un concepteur narratif avec un talent pour créer des histoires captivantes et des dialogues percutants. Avec un parcours en écriture créative et en design de jeux, il a contribué à des titres comme BioShock Infinite et The Outer Worlds. Son objectif est de créer des récits riches qui résonnent avec les joueurs, et il est enthousiaste à l'idée d'explorer de nouvelles techniques de narration chez Reubot Studio.",
+  },
+  {
+    id: 5,
+    lastName: "Martin",
+    name: "Claire",
+    img: female2,
+    role: "Artiste 3D",
+    text: "Claire est une artiste 3D talentueuse spécialisée dans la modélisation et l'animation de personnages. Avec un parcours en arts plastiques et en médias numériques, elle a contribué à plusieurs titres AAA, dont Final Fantasy XV et Horizon Zero Dawn. Sa vision artistique et son attention aux détails aident à donner vie aux personnages, et elle est impatiente de collaborer avec l'équipe pour créer des expériences de jeu mémorables chez Reubot Studio.",
+  },
+  {
+    id: 6,
+    lastName: "Moreau",
+    name: "Sophie",
+    img: female3,
+    role: "Conceptrice sonore",
+    text: "Sophie est une conceptrice sonore et compositrice passionnée par la création d'expériences audio immersives dans les jeux. Elle a travaillé sur des titres indépendants et des grandes productions, y compris Celeste et The Witcher 3. Sa capacité à mélanger les paysages sonores avec le gameplay renforce l'impact émotionnel des jeux sur lesquels elle travaille, et elle est impatiente de créer des expériences audio uniques chez Reubot Studio.",
+  },
+  {
+    id: 7,
+    lastName: "Garnier",
+    name: "Chloé",
+    img: female4,
+    role: "Spécialiste du marketing",
+    text: "Chloé est une spécialiste du marketing axée sur l'engagement communautaire et le développement de la marque. Avec une expérience dans les secteurs du jeu et de la technologie, elle a lancé avec succès des campagnes pour des jeux comme Overwatch et Fortnite. Sa passion pour la connexion avec les joueurs et la création de communautés dynamiques guide son travail chez Reubot Studio.",
+  },
+  {
+    id: 8,
+    lastName: "Boucher",
+    name: "Élodie",
+    img: female5,
+    role: "Testeur QA",
+    text: "Élodie est une testeur QA (Assurance Qualité) avec une attention particulière aux détails et un engagement à garantir la qualité des jeux. Avec plus de 5 ans d'expérience dans le domaine, elle a travaillé sur plusieurs projets, s'assurant que chaque aspect du jeu fonctionne parfaitement avant sa sortie. Sa passion pour les jeux vidéo et son esprit critique font d'elle un membre essentiel de l'équipe chez Reubot Studio.",
+  },
+  {
+    id: 9,
+    lastName: "Rousseau",
+    name: "Thomas",
+    img: male4,
+    role: "Designer de niveaux",
+    text: "Thomas est un designer de niveaux créatif avec une forte expérience dans la conception d'environnements de jeu immersifs. Il a travaillé sur des titres variés, allant des jeux de plateforme aux RPG. Sa capacité à créer des niveaux qui défient et engagent les joueurs est reconnue dans l'industrie, et il est enthousiaste à l'idée de contribuer à des projets innovants chez Reubot Studio.",
+  },
+];
+export default Team;
