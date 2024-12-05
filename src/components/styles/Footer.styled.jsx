0c91fd35a0e260b64a2e86f865ea2ecdc6c96be5
Remy Charpentier <remycha@msn.com>
2024-12-05 16:30:59 +0100

    correction footer brush et 1er bloc

--- src/components/styles/Footer.styled.jsx
+++ src/components/styles/Footer.styled.jsx
@@ -2,39 +2,66 @@ import styled from "styled-components";
 
 export const StyledFooter = styled.div`
   position: relative;
+
   #row-footer {
     margin-top: 16px;
   }
-  span {
+
+  span,
+  p {
     font-weight: bold;
   }
+
   #footer-logo {
     border-radius: 10px;
-    width: 136px;
+    width: 167px;
     margin-top: 5px;
   }
+
+  .company-text {
+    position: absolute;
+    bottom: -22px;
+    left: 78px;
+    a,
+    span {
+      position: relative;
+      top: 5px;
+    }
+  }
+
   #footer-company-name {
     font-size: 25px;
+    position: relative;
+    z-index: 1;
   }
+
+  .company-container {
+    position: relative;
+  }
+
   .brush-img {
     position: absolute;
-    top: 10.5vh;
-    left: 18vh;
+    top: -62px;
+    left: -105px;
     z-index: -1;
-    width: 400px;
+    width: 410px;
   }
+
   #div-reseaux img {
     margin-right: 10px;
     width: 50px;
   }
+
   .adresse {
     img {
       width: 50px;
     }
   }
+
   .translate ul {
     display: flex;
   }
+
   .flag {
     width: 50px;
     margin-right: 3px;
--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,4 +1,3 @@
-// import logo from "../../assets/img/logo.jpeg";
 import logo2 from "../../assets/img/logo/logo2.png";
 import location from "../../assets/img/footer/location_logo.png";
 import linkedin from "../../assets/img/footer/linkedin.png";
@@ -14,26 +13,24 @@ import Col from "react-bootstrap/Col";
 
 import { StyledFooter } from "../styles/Footer.styled";
 
-const companyName = "Reubot Studio";
+const nomEntreprise = "Reubot Studio";
 
 export default function Footer() {
   return (
-    <>
-      <StyledFooter>
-        <Container>
-          <Row id="row-footer">
-            <Col>
-              <a
-                href="https://www.canva.com/design/DAGWcQ7DDOY/gRMK4gLjBKwGr9lV2gcDsw/view?utm_content=DAGWcQ7DDOY&utm_campaign=designshare&utm_medium=link&utm_source=editor"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={logo2} alt="" id="footer-logo" />
-              </a>
-              <br />
-              <span>
-                Copyright © 2024
-                <br />
+    <StyledFooter>
+      <Container>
+        <Row id="row-footer">
+          <Col>
+            <a
+              href="https://www.canva.com/design/DAGWcQ7DDOY/gRMK4gLjBKwGr9lV2gcDsw/view?utm_content=DAGWcQ7DDOY&utm_campaign=designshare&utm_medium=link&utm_source=editor"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={logo2} alt="Logo de l'entreprise" id="footer-logo" />
+            </a>
+            <br />
+            <div className="company-container">
+              <div className="company-text">
                 <a
                   href="https://www.canva.com/design/DAGWcQ7DDOY/gRMK4gLjBKwGr9lV2gcDsw/view?utm_content=DAGWcQ7DDOY&utm_campaign=designshare&utm_medium=link&utm_source=editor"
                   target="_blank"
@@ -41,62 +38,67 @@ export default function Footer() {
                   className="company-name"
                   id="footer-company-name"
                 >
-                  {companyName}
+                  {nomEntreprise}
                 </a>
-                <img src={brush} alt="" className="brush-img" />
-                <br />
-                Video Games Development
-              </span>
-            </Col>
-            <Col className="adresse">
-              <h3>Adresse</h3>
+                <span> &copy; 2024</span>
+                <p>Développement de jeux vidéo</p>
+              </div>
+              <img src={brush} alt="Pinceau" className="brush-img" />
+            </div>
+            <br />
+          </Col>
+          <Col className="adresse">
+            <h3>Adresse</h3>
+            <a
+              href="https://www.google.fr/maps/place/45+Rue+Thiac,+33000+Bordeaux/@44.8437562,-0.58706,16z/data=!3m1!4b1!4m6!3m5!1s0xd5527e7258f8609:0x6f2ebbf2e4ca2a2a!8m2!3d44.8437524!4d-0.5844851!16s%2Fg%2F11c0zpx9tj?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={location} alt="Localisation" />
+            </a>
+            <br />
+            <span id="adresse-texte">45 rue Thiac, 33000 Bordeaux</span>
+          </Col>
+          <Col>
+            <h3>Réseaux sociaux</h3>
+            <div id="div-reseaux">
               <a
-                href="https://www.google.fr/maps/place/45+Rue+Thiac,+33000+Bordeaux/@44.8437562,-0.58706,16z/data=!3m1!4b1!4m6!3m5!1s0xd5527e7258f8609:0x6f2ebbf2e4ca2a2a!8m2!3d44.8437524!4d-0.5844851!16s%2Fg%2F11c0zpx9tj?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D"
+                href="https://www.linkedin.com/in/rémy-charpentier-a0ba4717a"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img src={location} alt="" />
+                <img src={linkedin} alt="LinkedIn" />
               </a>
-              <br />
-              <span id="adresse-texte">45 rue Thiac 33000 Bordeaux</span>
-            </Col>
-            <Col>
-              <div id="div-reseaux">
-                <h3>Réseaux</h3>
-                <a
-                  href="https://www.linkedin.com/in/rémy-charpentier-a0ba4717a"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={linkedin} alt="" />
-                </a>
-                <a
-                  href="https://github.com/remycharpentier"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={git} alt="" />
-                </a>
-                <a
-                  href="https://www.facebook.com/reubot.boy/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={facebook} alt="" />
-                </a>
-
-                <img src={twitter} alt="" />
-              </div>
-            </Col>
-            <Col className="translate">
-              <h3>Traduire</h3>
-              <Flag></Flag>
-            </Col>
-          </Row>
-          <br />
-          <hr />
-        </Container>
-      </StyledFooter>
-    </>
+              <a
+                href="https://github.com/remycharpentier"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={git} alt="GitHub" />
+              </a>
+              <a
+                href="https://www.facebook.com/reubot.boy/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={facebook} alt="Facebook" />
+              </a>
+              <a
+                href="https://twitter.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={twitter} alt="Twitter" />
+              </a>
+            </div>
+          </Col>
+          <Col className="translate">
+            <h3>Traduire</h3>
+            <Flag />
+          </Col>
+        </Row>
+        <hr />
+      </Container>
+    </StyledFooter>
   );
 }
